b08d695b07fc4cb606479c06b1eab5e79243a627
MrFancyMonocle <ryanforan@gmail.com>
2016-03-09 10:51:45 -0500

    styling: changing base input padding

--- src/containers/Buckets/editBucket.js
+++ src/containers/Buckets/editBucket.js
@@ -120,29 +120,72 @@ export default class Bucket extends Component {
     return (
       <section>
 		    <div className="container">
-			   <div className="row">
-			     <div className="col-xs-12">
+          <div className="row">
+            <div className="col-xs-12">
 
-				      <div className="row">
-					     <div className="col-sm-12">
-						      { !query.new  && <h1 className="title pull-left">Edit Bucket</h1> }
-                  { query.new  && <h1 className="title pull-left">Create Bucket</h1> }
-                  { !query.new  && <a href="javascript:void(0)" onClick={this.destroy.bind(this)} className="btn btn-action pull-right btn-red">Delete Bucket</a> }
-                  { !query.new  && <a href="javascript:void(0)" onClick={this.addFile.bind(this)} style={{marginRight:'12px'}} className="btn btn-action pull-right btn-transparent">Add File</a> }
-                  { !query.new  && <input type="file" onChange={this.inputFile.bind(this)} style={{display:'none'}} id="filePicker"/> }
+              <div className="row">
+                <div className="col-sm-12">
+						      <h1 className="title pull-left">Edit Bucket</h1>
 
-					     </div>
-				      </div>
+                  <a href="javascript:void(0)" onClick={this.destroy.bind(this)} className="btn btn-action pull-right btn-red">Delete Bucket</a>
+                  <a href="javascript:void(0)" onClick={this.addFile.bind(this)} style={{marginRight:'12px'}} className="btn btn-action pull-right btn-transparent">Add File</a>
+                  <input type="file" onChange={this.inputFile.bind(this)} style={{display:'none'}} id="filePicker"/>
+
+                </div>
+              </div>
+
               <form>
                 <div className="row">
-					       <div className="col-sm-12">
+                  <div className="col-sm-12">
 						        <div className="content">
-						  	     <label htmlFor="name">Bucket Name</label>
-						  	     <input type="text" name="name" placeholder="Bucket Name" {...this.props.fields.name}/>
+                      <div className="form-group">
+                        <label htmlFor="name">Bucket Name</label>
+                        <input type="text" className="form-control" name="name" placeholder="Bucket Name" {...this.props.fields.name}/>
+                      </div>
 						        </div>
-					       </div>
+                  </div>
+                </div>
+
+				        <div className="row">
+                  <div className="col-xs-6">
+						        <Link to="/dashboard" className="btn btn-block btn-transparent">Go Back</Link>
+                  </div>
+
+                  <div className="col-xs-6">
+						        <a href="javascript:void(0)" onClick={this.updateBucket.bind(this)} className="btn btn-block btn-green btn-create-bucket">Update Bucket</a>
+                  </div>
 				        </div>
-{/*
+              </form>
+
+              <div className="row">
+                <div className="col-sm-12">
+                  <div className="content" style={{overflow:'hidden'}}>
+
+                    <div className="form-group">
+                      <label>File Hash</label>
+                      <input type="text" className="form-control" name="filehash" placeholder="Paste a File Hash" {...this.props.fields.fileHash}/>
+                      <a href="javascript:void(0)" onClick={this.previewHash.bind(this)} style={{marginLeft:'12px'}} className="btn btn-action pull-left btn-transparent">Retrieve</a>
+                    </div>
+
+                  </div>
+
+                  { this.props.bucket.fileURI &&
+                  <div className="content">
+                    <iframe style={{minHeight: '800px', width: '100%'}} src={this.props.bucket.fileURI}></iframe>
+                  </div>
+                  }
+
+                </div>
+              </div>
+
+			     </div>
+			   </div>
+		    </div>
+      </section>
+    );
+  }
+
+/*
 				        <div className="row">
 					       <div className="col-sm-12">
 						        <div className="content" id="publicKeys">
@@ -152,41 +195,7 @@ export default class Bucket extends Component {
 						        </div>
 					       </div>
 				        </div>
-*/}
-				        <div className="row">
-					       <div className="col-xs-6">
-						        <Link to="/dashboard" className="btn btn-block btn-transparent">Go Back</Link>
-					       </div>
-                  { query.new &&
-                  <div className="col-xs-6">
-						        <a href="javascript:void(0)" onClick={this.createBucket.bind(this)} className="btn btn-block btn-green btn-create-bucket">Save Bucket</a>
-					       </div> }
-                  { this.props.params.bucketId &&
-                  <div className="col-xs-6">
-						        <a href="javascript:void(0)" onClick={this.updateBucket.bind(this)} className="btn btn-block btn-green btn-create-bucket">Update Bucket</a>
-					       </div> }
-				        </div>
-              </form>
+*/
 
-              { !query.new && <div className="row">
-               <div className="col-sm-12">
-                  <div className="content" style={{overflow:'hidden'}}>
-                   <label>File Hash</label>
-                   <input type="text" name="filehash" placeholder="Paste a File Hash" {...this.props.fields.fileHash}/>
-                      <a href="javascript:void(0)" onClick={this.previewHash.bind(this)} style={{marginLeft:'12px'}} className="btn btn-action pull-left btn-transparent">Retrieve</a>
-                  </div>
-                  { this.props.bucket.fileURI &&
-                    <div className="content">
-                       <iframe style={{minHeight: '800px', width: '100%'}} src={this.props.bucket.fileURI}></iframe>
-                    </div>
-                  }
-               </div>
-              </div> }
 
-			     </div>
-			   </div>
-		    </div>
-      </section>
-    );
-  }
 };
