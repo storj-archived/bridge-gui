8610a78bd348d228b7a1254567b4c87d0c687a81
Tome Boshevski <tome@boshevski.com>
2016-03-24 23:43:12 +0100

    Update bucket list

--- src/components/BucketList/bucketListItem.js
+++ src/components/BucketList/bucketListItem.js
@@ -2,23 +2,25 @@ import React from 'react';
 
 const BucketListItem = (props) => {
   return (
-    <tr>
-      <td className="bucket-icon hidden-xs">
-        <img src="img/icon-bucket.svg" alt=""/>
-      </td>
-      <td className="bucket-label">
-        <a onClick={props.onClick}>{props.name}</a>
-      </td>
-      <td className="bucket-storage">
-        {props.storage} GB
-      </td>
-      <td className="bucket-transfer">
-        {props.transfer} GB
-      </td>
-      <td className="bucket-status">
-        {props.status}
-      </td>
-    </tr>
+    <a onClick={props.onClick}>
+      <tr className="clickable-row">
+        <td className="bucket-icon hidden-xs">
+          <img src="img/icon-bucket.svg" alt=""/>
+        </td>
+        <td className="bucket-label">
+          <a onClick={props.onClick}>{props.name}</a>
+        </td>
+        <td className="bucket-storage">
+          {props.storage} GB
+        </td>
+        <td className="bucket-transfer">
+          {props.transfer} GB
+        </td>
+        <td className="bucket-status">
+          {props.status}
+        </td>
+      </tr>
+    </a>
   );
 };
 /*
