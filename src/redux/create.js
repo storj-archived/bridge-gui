3aa008ad9326c0ae1a88e0d2c49de2d38936975d
Bryan White <bryanchriswhite@gmail.com>
2016-07-11 08:13:32 +0200

    Upgrade to babel 6 and cleanup:
    + update package.json and .babelrc
    + cleanup server.babel.js (remove unnecessary manual loading of .babelrc)
    + use `import` instead of `require` in src/redux/create.js for proper ES6 module compatibility
    + cleanup src/redux/create.js & switch to documented method for using `createStore` and `applyMiddleware`
    
    for additional info regarding the babel upgrade reference https://medium.com/@kentcdodds/misunderstanding-es6-modules-upgrading-babel-tears-and-a-solution-ad2d5ab93ce0#.mz5lgf7pt

--- src/redux/create.js
+++ src/redux/create.js
@@ -1,11 +1,13 @@
 import { createStore as _createStore, applyMiddleware, compose } from 'redux';
 import createMiddleware from './middleware/clientMiddleware';
+import reducer from './modules/reducer';
 
 export default function createStore(client, data) {
   const middleware = [createMiddleware(client)];
 
+/*
   let finalCreateStore;
-/*  if (__DEVELOPMENT__ && __CLIENT__ && __DEVTOOLS__) {
+  if (__DEVELOPMENT__ && __CLIENT__ && __DEVTOOLS__) {
     const { persistState } = require('redux-devtools');
     const DevTools = require('../containers/DevTools/index');
     finalCreateStore = compose(
@@ -13,12 +15,12 @@ export default function createStore(client, data) {
       window.devToolsExtension ? window.devToolsExtension() : DevTools.instrument(),
       persistState(window.location.href.match(/[?&]debug_session=([^&]+)\b/))
     )(_createStore);
-  } else { */
+  } else {
     finalCreateStore = applyMiddleware(...middleware)(_createStore);
-//  }
+  }
+*/
 
-  const reducer = require('./modules/reducer');
-  const store = finalCreateStore(reducer, data);
+  const store = _createStore(reducer, applyMiddleware(...middleware));
 
   return store;
 }
