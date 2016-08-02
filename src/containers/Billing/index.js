4158e0e016dd9c3646c4b176f583bab8f8067a83
Dylan Lott <lott.dylan@gmail.com>
2016-08-02 13:38:44 -0400

    added rough HTML in for billing interface

--- src/containers/Billing/index.js
+++ src/containers/Billing/index.js
@@ -15,18 +15,214 @@ import Loader from 'react-loader';
 //   })
 // )
 
-export default class Billing extends Component {
+export default className Billing extends Component {
   render() {
     return (
+      <div>
+
       <section>
-        <div className="container">
-          <div className="row">
-            <div className="col-xs-12">
-              <h1 className="title pull-left">Billing</h1>
+        <div classNameName="container">
+          <div classNameName="row">
+            <div classNameName="col-xs-12">
+              <h1 classNameName="title pull-left">Billing</h1>
             </div>
           </div>
         </div>
       </section>
+
+      <section>
+		<div className="container">
+			<div className="row">
+				<div className="col-xs-12 col-sm-6">
+					<h2 className="title">Your Balance</h2>
+					<div className="content">
+						<div className="row">
+							<div className="col-xs-6">
+								<p className="text-muted">Credit Balance</p>
+								<h2 className="mb0 blue"><b>$32.58</b></h2>
+							</div>
+							<div className="col-xs-6 text-right">
+								<a href="" className="btn btn-green btn-action">Add Credit</a>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div className="col-xs-12 col-sm-6">
+					<h2 className="title">This Month</h2>
+					<div className="content">
+						<div className="row">
+							<div className="col-xs-6">
+								<p className="text-muted">Current Usage</p>
+								<h2 className="mb0 blue"><b>$12.44</b></h2>
+							</div>
+							<div className="col-xs-6 text-right">
+								<a href="" className="btn btn-action">View Usage</a>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+
+	<section>
+		<div className="container">
+	      	<div className="row">
+				<div className="col-xs-12">
+					<h2 className="title">Add Credit Card</h2>
+				</div>
+			</div>
+			<div className="row">
+				<div className="col-xs-12">
+					<div className="content">
+						<form action="" accept-charset="UTF-8" method="post">
+							<input name="utf8" type="hidden" value="✓" />
+							<input type="hidden" name="authenticity_token" value="" />
+
+							<div className="spacer10"></div>
+
+							<fieldset>
+								<legend>Your Details</legend>
+								<div className="row">
+									<div className="col-sm-6">
+										<input placeholder="First Name" data-required="true" data-stripe="first_name" type="text" name="user_payment_profile[first_name]" id="user_payment_profile_first_name" />
+									</div>
+									<div className="col-sm-6">
+										<input placeholder="Last Name" data-required="true" data-stripe="last_name" type="text" name="user_payment_profile[last_name]" id="user_payment_profile_last_name" />
+									</div>
+								</div>
+								<div className="row">
+									<div className="col-sm-6">
+										<input placeholder="Phone Number" data-required="true" type="tel" name="user_payment_profile[phone_number]" id="user_payment_profile_phone_number" />
+									</div>
+									<div className="col-sm-6">
+										<input placeholder="Street Address" data-required="true" data-stripe="address_line1" type="text" name="user_payment_profile[address]" id="user_payment_profile_address" />
+									</div>
+								</div>
+								<div className="row">
+									<div className="col-sm-6">
+										<input placeholder="City" data-required="true" data-stripe="address_city" type="text" name="user_payment_profile[city]" id="user_payment_profile_city" />
+									</div>
+									<div className="col-sm-6">
+										<input placeholder="State" data-stripe="address_state" type="text" name="user_payment_profile[state]" id="user_payment_profile_state" />
+									</div>
+								</div>
+								<div className="row">
+									<div className="col-sm-6">
+										<input placeholder="Postal Code" data-stripe="address_zip" type="number" name="user_payment_profile[zip]" id="user_payment_profile_zip" />
+									</div>
+									<div className="col-sm-6">
+										<select name="user_payment_profile[country]" id="user_payment_profile_country" title="Select your country">
+											<option value="">The World</option>
+										</select>
+									</div>
+								</div>
+							</fieldset>
+
+							<div className="spacer35"></div>
+
+							<fieldset>
+								<legend>Credit Card Details</legend>
+								<div className="row">
+									<div className="col-sm-6">
+										<input placeholder="Credit Card Number" className="Input--floatLabel cc-num FloatLabel-input" name="" data-required="true" data-stripe="number" type="text" id="user_payment_profile_credit_card_number" />
+									</div>
+									<div className="col-sm-6">
+										<input placeholder="CVV" className="Input--floatLabel cc-cvc FloatLabel-input" name="" data-required="true" data-stripe="cvc" type="text" id="user_payment_profile_cvv" />
+									</div>
+								</div>
+								<div className="row">
+									<div className="col-sm-6">
+										<input placeholder="Expires MM / YYYY" className="Input--floatLabel cc-exp FloatLabel-input" name="" data-required="true" data-stripe="expiry" type="text" id="user_payment_profile_expiration" />
+									</div>
+									<div className="col-sm-6">
+										<input placeholder="Please enter a name for this credit card" type="text" name="user_payment_profile[name]" id="user_payment_profile_name" />
+									</div>
+								</div>
+							</fieldset>
+
+							<div className="spacer20"></div>
+
+							<div className="row">
+								<div className="col-xs-12">
+									<input type="submit" name="submit" value="Save Credit Card" className="btn btn-block" />
+								</div>
+							</div>
+
+							<div className="spacer10"></div>
+						</form>
+
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+
+
+	<section>
+		<div className="container">
+			<div className="row">
+				<div className="col-xs-12">
+					<h2 className="title">Billing History</h2>
+	      		</div>
+	      	</div>
+	      	<div className="row">
+				<div className="col-xs-12">
+					<div className="table-responsive content">
+						<table className="table table-hover">
+							<thead>
+								<tr>
+									<th>Date</th>
+									<th>Description</th>
+									<th>Amount</th>
+								</tr>
+							</thead>
+								<tbody>
+								<tr className="clickable-row">
+									<td>
+										Jan 25, 2016
+									</td>
+									<td>
+										Add Credit
+									</td>
+									<td>
+										$50
+									</td>
+								</tr>
+
+								<tr className="clickable-row">
+									<td>
+										Jan 01, 2016
+									</td>
+									<td>
+										Invoice for December 2015
+									</td>
+									<td>
+										$15.41
+									</td>
+								</tr>
+
+								<tr className="clickable-row">
+									<td>
+										Dec 25, 2016
+									</td>
+									<td>
+										Add Credit
+									</td>
+									<td>
+										$100
+									</td>
+								</tr>
+
+							</tbody>
+						</table>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+
+      </div>
     );
   }
 }
