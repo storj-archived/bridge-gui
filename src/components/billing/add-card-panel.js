fbaed18e01a272e42646e261cd31969311bf478e
Dylan Lott <lott.dylan@gmail.com>
2016-08-03 10:09:03 -0400

    working on card form more

--- src/components/billing/add-card-panel.js
+++ src/components/billing/add-card-panel.js
@@ -2,98 +2,117 @@ import React from 'react';
 import CountryDropdown from '../form-controls/country-dropdown';
 
 const AddCardPanel = () => {
-  return (
-    <section>
-      <div className="container">
-        <div className="row">
-          <div className="col-xs-12">
-            <h2 className="title">Add Credit Card</h2>
-          </div>
+
+return (
+  <section>
+    <div className="container">
+      <div className="row">
+        <div className="col-xs-12">
+          <h2 className="title">Add Credit Card</h2>
         </div>
-        <div className="row">
-          <div className="col-xs-12">
-            <div className="content">
-              <form action="" accept-charset="UTF-8" method="post">
-                <input name="utf8" type="hidden" value="✓" />
-                <input type="hidden" name="authenticity_token" value="" />
+      </div>
+      <div className="row">
+        <div className="col-xs-12">
+          <div className="content">
+            <form action="" accept-charset="UTF-8" method="post">
+              <input name="utf8" type="hidden" value="✓" />
+              <input type="hidden" name="authenticity_token" value="" />
 
-                <div className="spacer10"></div>
+              <div className="spacer10"></div>
 
-                <fieldset>
-                  <legend>Your Details</legend>
-                  <div className="row">
-                    <div className="col-sm-6">
-                      <input placeholder="First Name" data-required="true" data-stripe="first_name" type="text" name="user_payment_profile[first_name]" id="user_payment_profile_first_name" />
-                    </div>
-                    <div className="col-sm-6">
-                      <input placeholder="Last Name" data-required="true" data-stripe="last_name" type="text" name="user_payment_profile[last_name]" id="user_payment_profile_last_name" />
-                    </div>
-                  </div>
-                  <div className="row">
-                    <div className="col-sm-6">
-                      <input placeholder="Phone Number" data-required="true" type="tel" name="user_payment_profile[phone_number]" id="user_payment_profile_phone_number" />
-                    </div>
-                    <div className="col-sm-6">
-                      <input placeholder="Street Address" data-required="true" data-stripe="address_line1" type="text" name="user_payment_profile[address]" id="user_payment_profile_address" />
-                    </div>
-                  </div>
-                  <div className="row">
-                    <div className="col-sm-6">
-                      <input placeholder="City" data-required="true" data-stripe="address_city" type="text" name="user_payment_profile[city]" id="user_payment_profile_city" />
-                    </div>
-                    <div className="col-sm-6">
-                      <input placeholder="State" data-stripe="address_state" type="text" name="user_payment_profile[state]" id="user_payment_profile_state" />
-                    </div>
-                  </div>
-                  <div className="row">
-                    <div className="col-sm-6">
-                      <input placeholder="Postal Code" data-stripe="address_zip" type="number" name="user_payment_profile[zip]" id="user_payment_profile_zip" />
-                    </div>
-                    <div className="col-sm-6">
-                      <CountryDropdown name="user_payment_profile[country]" id="user_payment_profile_country" title="Select your country" />
-                    </div>
-                  </div>
-                </fieldset>
-
-                <div className="spacer35"></div>
-
-                <fieldset>
-                  <legend>Credit Card Details</legend>
-                  <div className="row">
-                    <div className="col-sm-6">
-                      <input placeholder="Credit Card Number" className="Input--floatLabel cc-num FloatLabel-input" name="" data-required="true" data-stripe="number" type="text" id="user_payment_profile_credit_card_number" />
-                    </div>
-                    <div className="col-sm-6">
-                      <input placeholder="CVV" className="Input--floatLabel cc-cvc FloatLabel-input" name="" data-required="true" data-stripe="cvc" type="text" id="user_payment_profile_cvv" />
-                    </div>
-                  </div>
-                  <div className="row">
-                    <div className="col-sm-6">
-                      <input placeholder="Expires MM / YYYY" className="Input--floatLabel cc-exp FloatLabel-input" name="" data-required="true" data-stripe="expiry" type="text" id="user_payment_profile_expiration" />
-                    </div>
-                    <div className="col-sm-6">
-                      <input placeholder="Please enter a name for this credit card" type="text" name="user_payment_profile[name]" id="user_payment_profile_name" />
-                    </div>
-                  </div>
-                </fieldset>
-
-                <div className="spacer20"></div>
-
+              <fieldset>
+                <legend>Your Details</legend>
                 <div className="row">
-                  <div className="col-xs-12">
-                    <input type="submit" name="submit" value="Save Credit Card" className="btn btn-block" />
+                  <div className="form-group">
+                    <div className="col-sm-6">
+                      <input className="form-control" placeholder="First Name" type="text" name="fname" autoComplete="given-name" />
+                    </div>
+                  </div>
+                  <div className="form-group">
+                    <div className="col-sm-6">
+                      <input className="form-control" placeholder="Last Name" type="text" name="lname" autoComplete="family-name" />
+                    </div>
                   </div>
                 </div>
+                <div className="row">
+                  <div className="form-group">
+                    <div className="col-sm-6">
+                      <input className="form-control" placeholder="Phone Number"  type="tel" name="phone" autoComplete="tel"/>
+                    </div>
+                  </div>
+                  <div className="form-group">
+                    <div className="col-sm-6">
+                      <input className="form-control" placeholder="Street Address" type="text" name="address" autoComplete="street-address"/>
+                    </div>
+                  </div>
+                </div>
+                <div className="row">
+                  <div className="form-group">
+                    <div className="col-sm-6">
+                      <input className="form-control" placeholder="City" type="text" name="city" autoComplete="address-level2"/>
+                    </div>
+                  </div>
+                  <div className="form-group">
+                    <div className="col-sm-6">
+                      <input className="form-control" placeholder="State" type="text" name="state" autoComplete="address-level1"/>
+                    </div>
+                  </div>
+                </div>
+                <div className="row">
+                  <div className="form-group">
+                    <div className="col-sm-6">
+                      <input className="form-control" placeholder="Postal Code" type="number" name="zip" autoComplete="postal-code"/>
+                    </div>
+                  </div>
+                  <div className="form-group">
+                    <div className="col-sm-6">
+                      <CountryDropdown name="country" autoComplete="country" title="Select your country" />
+                    </div>
+                  </div>
+                </div>
+              </fieldset>
 
-                <div className="spacer10"></div>
-              </form>
+              <div className="spacer35"></div>
 
-            </div>
+              <fieldset>
+                <legend>Credit Card Details</legend>
+                <div className="row">
+                  <div className="col-sm-6">
+                    <input className="form-control" placeholder="Credit Card Number" name="cc-number" type="text" />
+                  </div>
+                  <div className="col-sm-6">
+                    <input className="form-control" placeholder="CVV" name="cc-name" type="text" />
+                  </div>
+                </div>
+                <div className="row">
+                  <div className="col-sm-6">
+                    <input className="form-control" placeholder="Expires MM / YYYY" name="cc-exp" type="text" />
+                  </div>
+                  <div className="col-sm-6">
+                    <input className="form-control" placeholder="Please enter a name for this credit card" type="text" name="cc-name" />
+                  </div>
+                </div>
+              </fieldset>
+
+              <div className="spacer20"></div>
+
+              <div className="row">
+                <div className="col-xs-12">
+                  <input type="submit" name="submit" value="Save Credit Card" className="btn btn-block" />
+                </div>
+              </div>
+
+              <div className="spacer10"></div>
+            </form>
+
           </div>
         </div>
       </div>
-    </section>
-  )
+    </div>
+  </section>
+
+)
+
 }
 
 export default AddCardPanel;
--- src/containers/billing/add-card-form.js
+++ src/containers/billing/add-card-form.js
@@ -1,15 +1,22 @@
-import React, {Component} from 'react';
+import React, {Component, PropTypes} from 'react';
 import {reduxForm} from 'redux-form';
 import AddCardPanel from '../../components/billing/add-card-panel';
 
+@reduxForm({
+    form: 'CreditCard',
+    fields: ['firstName', 'lastName', '' ]
+})
+
 export default class AddCardForm extends Component {
   static propTypes = {
-
+    fields: PropTypes.object.isRequired,
+    handleSubmit: PropTypes.func.isRequired
   }
 
   render() {
-      return (
-        <AddCardPanel />
-      )
+    const {fields} = this.props;
+    return (
+      <AddCardPanel fields={fields} />
+    )
   }
 }
