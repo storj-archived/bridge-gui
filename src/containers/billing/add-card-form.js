c6e5e906288678aca70c6902b07d4e85c3550a1d
Bryan White <bryanchriswhite@gmail.com>
2016-08-08 17:34:12 +0200

    add graphiql to dev-server

--- src/containers/billing/add-card-form.js
+++ src/containers/billing/add-card-form.js
@@ -1,5 +1,4 @@
 import React, {Component, PropTypes} from 'react';
-// import {connect} from 'react-redux';
 import {connect} from 'react-apollo';
 import gql from 'graphql-tag';
 import {reduxForm} from 'redux-form';
