7044c5e2144c62f9572931d0da6c650e27d4efc3
Bryan White <bryanchriswhite@gmail.com>
2016-08-11 17:02:14 +0200

    is the nightmare over?

--- src/containers/Billing/index.js
+++ src/containers/Billing/index.js
@@ -1,21 +1,47 @@
 import React, {Component} from 'react';
-import {connect} from 'react-redux';
+import {connect} from 'react-apollo';
+import gql from 'graphql-tag';
 import BalancePanel from 'components/billing/balance-panel';
 import UsagePanel from 'components/billing/usage-panel';
 import AddCardForm from 'containers/billing/add-card-form';
-import TransactionsContainer from 'containers/billing/transactions-container';
-// @connect(
-//   state => ({
-//     buckets: state.bucketList
-//   }),
-//   dispatch => ({
-//     load: () => dispatch(bucketListActions.load())
-//   })
-// )
+import TransactionsList from 'components/billing/transactions-list';
+
+const mapQueriesToProps = () => {
+  return {
+    data: {
+      query: gql`query getTransactions($id: String!) {
+        user(id: $id) {
+          credits {
+            id,
+            amount,
+            created
+          },
+          debits {
+            id,
+            amount,
+            created
+          }
+        }
+      }`,
+      variables: {
+        id: 'user1@example.com'
+      }
+    }
+  };
+};
+
+@connect({
+  mapQueriesToProps
+})
 
 export default class Billing extends Component {
   render() {
-    const addCreditHandler = () => {};
+    const {user} = this.props.data;
+    const transactions = user ?
+      [...user.credits, ...user.debits] : [];
+
+    const addCreditHandler = () => {
+    };
     const amount = '$32.48';
     const linkParams = '/dashboard/billing/usage';
 
@@ -32,24 +58,24 @@ export default class Billing extends Component {
           </div>
         </section>
 
-      <section>
-		<div className="container">
-			<div className="row">
-				<div className="col-xs-12 col-sm-6">
+        <section>
+          <div className="container">
+            <div className="row">
+              <div className="col-xs-12 col-sm-6">
 
-        <BalancePanel amount={amount} addCreditHandler={addCreditHandler} />
+                <BalancePanel amount={amount} addCreditHandler={addCreditHandler}/>
 
-				</div>
-				<div className="col-xs-12 col-sm-6">
-					<UsagePanel amount={amount} linkParams={linkParams}/>
-				</div>
-			</div>
-		</div>
-	</section>
+              </div>
+              <div className="col-xs-12 col-sm-6">
+                <UsagePanel amount={amount} linkParams={linkParams}/>
+              </div>
+            </div>
+          </div>
+        </section>
 
-  <AddCardForm />
+        <AddCardForm />
 
-  <TransactionsContainer />
+        <TransactionsList transactions={transactions}/>
 
       </div>
     );
