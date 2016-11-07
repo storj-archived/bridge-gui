0773749153fd4effe6ad2c2d0f1fcad51b08e45f
Barbara Liau <bj.liau@gmail.com>
2016-11-07 08:50:08 -0700

    PropTypes, ES6+ style

--- src/containers/app/index.js
+++ src/containers/app/index.js
@@ -4,23 +4,23 @@ import config from 'config';
 import 'containers/app/app.scss';
 
 class App extends Component {
+  static propTypes = {
+    navComponent: React.propTypes.element,
+    mainComponent: React.propTypes.element
+  }
+
   render() {
     // const styles = require('./app.scss');
     return (
-      <div style={{height:'100%'}}>
+      <div style={{ height:'100%' }}>
         <Helmet {...config.app.head}/>
-        <div style={{height:'100%'}}>
-        	{ this.props.navComponent }
-        	{ this.props.mainComponent }
+        <div style={{ height:'100%' }}>
+        	{this.props.navComponent}
+        	{this.props.mainComponent}
         </div>
       </div>
     );
   }
 }
-
-App.propTypes = {
-  navComponent: React.propTypes.element,
-  mainComponent: React.propTypes.element
-};
 
 export default App;
