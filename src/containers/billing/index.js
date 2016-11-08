1cbb0e8bf9d6cbdbfdd661daa86eed5ec616b770
Barbara Liau <bj.liau@gmail.com>
2016-11-08 13:03:44 -0700

    Linting, add propTypes, refactor repetitive code

--- src/containers/billing/index.js
+++ src/containers/billing/index.js
@@ -1,5 +1,5 @@
-import React, {Component} from 'react';
-import {connect} from 'react-apollo';
+import React, { Component, PropTypes } from 'react';
+import { connect } from 'react-apollo';
 import gql from 'graphql-tag';
 import moment from 'moment';
 import BalancePanel from 'components/billing/balance-panel';
@@ -7,8 +7,8 @@ import PaymentInfoPanel from 'components/billing/payment-info-panel';
 import UsagePanel from 'components/billing/usage-panel';
 import AddCardForm from 'containers/billing/add-card-form';
 import TransactionsList from 'components/billing/transactions-list';
+import { setBalance, setUsage } from 'redux/modules/transaction-group';
 import 'containers/billing/billing.scss';
-import {setBalance, setUsage} from 'redux/modules/transaction-group';
 
 const transactionRangeQuery =
   gql`query usageTransactions($startDate: String!, $endDate: String!) {
@@ -58,8 +58,8 @@ const mapQueriesToProps = () => {
         }
       }`
     }
-  }
-}
+  };
+};
 
 const mapMutationsToProps = () => {
   return {
@@ -82,8 +82,8 @@ const mapDispatchToProps = {
   setUsage
 };
 
-const mapStateToProps = ({transactionGroup: {balance, usage}}) => {
-  return {balance, usage};
+const mapStateToProps = ({ transactionGroup: { balance, usage } }) => {
+  return { balance, usage };
 };
 
 @connect({
@@ -94,16 +94,32 @@ const mapStateToProps = ({transactionGroup: {balance, usage}}) => {
 })
 
 export default class Billing extends Component {
+  static propTypes = {
+    query: PropTypes.func,
+    setBalance: PropTypes.func,
+    setUsage: PropTypes.func,
+    paymentProcessor: PropTypes.object,
+    transactions: PropTypes.object,
+    mutations: PropTypes.func
+  }
+
   componentWillReceiveProps(nextProps) {
     const {balance, usage} = nextProps;
     // TODO: Use apollo query observables instead of promises
     if (typeof balance !== 'undefined' || typeof usage !== 'undefined') {
-        return;
+      return;
     }
-    const {startDate: balanceStartDate, endDate: balanceEndDate} = this.getBalanceRange();
-    const {startDate: usageStartDate, endDate: usageEndDate} = this.getUsageRange();
+    const {
+      startDate: balanceStartDate,
+      endDate: balanceEndDate
+    } = this.getBalanceRange();
+    const {
+      startDate: usageStartDate,
+      endDate: usageEndDate
+    } = this.getUsageRange();
 
-    if (!balanceStartDate || !balanceEndDate || !usageStartDate || !usageEndDate) {
+    if (!balanceStartDate || !balanceEndDate
+       || !usageStartDate || !usageEndDate) {
       return null;
     }
     // TODO: Use Apollo query observables instead of promises
@@ -124,53 +140,78 @@ export default class Billing extends Component {
       }
     });
 
-    balancePromise.then(({data: {credits, debits}, loading}) => {
+    balancePromise.then(({ data: { credits, debits }}) => {
       const balance = this.calculateBalance(credits, debits);
       this.props.setBalance(balance);
     });
 
-    usagePromise.then(({data: {credits, debits}}) => {
+    usagePromise.then(({ data: { credits, debits }}) => {
       const usage = this.calculateBalance(credits, debits);
       this.props.setUsage(usage);
     });
   }
 
   getBalanceRange() {
-    const {loading, paymentProcessor} = this.props.paymentProcessor;
-    const billingDate = (loading || !paymentProcessor) ? (new Date).getDate() : paymentProcessor.billingDate;
-    const today = new Date();
-    const daysInMonth = (new Date(today.getFullYear(), (today.getMonth() - 1), 0)).getDate();
-    const startDayOfMonth = (billingDate > daysInMonth) ? daysInMonth : billingDate;
-    const startDate = Date.parse(new Date(
-      today.getFullYear(),
-      (today.getMonth() - 1),
-      startDayOfMonth
-    ));
-    const endDate = (moment(startDate).add('1', 'month').unix() * 1000);
+    const { loading, paymentProcessor } = this.props.paymentProcessor;
+    const dates = getDates(loading, paymentProcessor);
 
     return {
-      startDate,
-      endDate
+      startDate: dates.startDate,
+      endDate: dates.endDate
     };
   }
 
   getUsageRange() {
-    const {loading, paymentProcessor} = this.props.paymentProcessor;
-    const billingDate = (loading || !paymentProcessor) ? (new Date).getDate() : paymentProcessor.billingDate;
-    const today = new Date();
-    const daysInMonth = (new Date(today.getFullYear(), today.getMonth(), 0)).getDate();
-    const startDayOfMonth = (billingDate > daysInMonth) ? daysInMonth : billingDate;
-    const startDate = Date.parse(new Date(
-      today.getFullYear(),
-      (today.getMonth()),
-      startDayOfMonth
-    ));
-    const endDate = (moment(startDate).add('1', 'month').unix() * 1000);
+    const { loading, paymentProcessor } = this.props.paymentProcessor;
+    const dates = getDates(loading, paymentProcessor);
 
     return {
-      startDate,
-      endDate
+      startDate: dates.startDate,
+      endDate: dates.endDate
     };
+  }
+
+  getPaymentInfo() {
+    const { loading } = this.props.paymentProcessor;
+    if (loading || !this.props.paymentProcessor.paymentProcessor) {
+      return null;
+    }
+    const { defaultCard } = this.props.paymentProcessor.paymentProcessor;
+    return defaultCard;
+  }
+
+  getTransactions() {
+    const { loading, credits, debits } = this.props.transactions;
+
+    if (loading || !credits || !debits) {
+      return [];
+    }
+
+    const creditMsg = 'payment - Thank you!';
+    const convertedCredits = convertUnits(credits, creditMsg);
+    const debitMsg = 'successful';
+    const convertedDebits = convertUnits(debits, debitMsg);
+    const transactions = [...convertedCredits, ...convertedDebits];
+
+    return transactions.sort((t1, t2) => (t2.timestamp - t1.timestamp));
+
+    function convertUnits(units, customMsg) {
+      units.map((unit) => {
+        const transaction = { ...unit };
+        const titleizedType = unit.type.replace(/^\w/, (word) => {
+          return word.toUpperCase();
+        });
+        transaction.amount = -unit.paid_amount;
+        transaction.description = `${titleizedType} ${customMsg}`;
+        transaction.timestamp = Date.parse(unit.created);
+        transaction.created = `${
+          moment(unit.created)
+          .utc()
+          .format('MMM DD, YYYY - HH:mm')} UTC
+        `;
+        return transaction;
+      });
+    }
   }
 
   calculateBalance(credits, debits) {
@@ -184,57 +225,10 @@ export default class Billing extends Component {
     return balance;
   }
 
-  getPaymentInfo() {
-    const {loading} = this.props.paymentProcessor;
-
-    if (loading || !this.props.paymentProcessor.paymentProcessor) {
-      return null;
-    }
-
-    const {defaultCard} = this.props.paymentProcessor.paymentProcessor;
-    return defaultCard;
-  }
-
-  getTransactions() {
-    const {loading, credits, debits} = this.props.transactions;
-    let transactions;
-
-    if (loading || !credits || !debits) {
-      return [];
-    }
-
-    const convertedCredits = credits.map((credit) => {
-      const transaction = {...credit};
-      transaction.amount = -credit.paid_amount;
-      const titleizedType = credit.type
-        .replace(/^\w/, (w) => (w.toUpperCase()));
-      transaction.description = `${titleizedType} payment - Thank you!`;
-      transaction.timestamp = Date.parse(credit.created);
-      transaction.created = `${moment(credit.created)
-        .utc().format('MMM DD, YYYY - HH:mm')} UTC`;
-      return transaction;
-    });
-
-    const convertedDebits = debits.map((debit) => {
-      const transaction = {...debit};
-      const titleizedType = debit.type
-        .replace(/^\w/, (w) => (w.toUpperCase()));
-      transaction.description = `${titleizedType} successful`;
-      transaction.timestamp = Date.parse(debit.created);
-      transaction.created = `${moment(debit.created)
-        .utc().format('MMM DD, YYYY - HH:mm')} UTC`;
-      return transaction;
-    });
-
-    transactions = [...convertedCredits, ...convertedDebits];
-
-    return transactions.sort((t1, t2) => (t2.timestamp - t1.timestamp));
-  }
-
   removeCard() {
-    const {removeCard} = this.props.mutations;
+    const { removeCard } = this.props.mutations;
     // TODO: use apollo watchquery correctly so we don't have to call `refetch`
-    const {refetch} = this.props.paymentProcessor;
+    const { refetch } = this.props.paymentProcessor;
 
     removeCard().then(() => (refetch()));
   }
@@ -259,21 +253,27 @@ export default class Billing extends Component {
           <div className="container">
             <div className="row">
               <div className="col-xs-12 col-sm-6">
-                <BalancePanel amount={this.props.balance}
-                              addCreditHandler={addCreditHandler}
-                              cardData={this.getPaymentInfo()}/>
+                <BalancePanel
+                  amount={this.props.balance}
+                  addCreditHandler={addCreditHandler}
+                  cardData={this.getPaymentInfo()}
+                />
               </div>
               <div className="col-xs-12 col-sm-6">
-                <UsagePanel amount={this.props.usage} linkParams={linkParams}/>
+                <UsagePanel
+                  amount={this.props.usage}
+                  linkParams={linkParams}
+                />
               </div>
             </div>
             <div className="row">
               <div className="col-xs-12">
-                { !this.getPaymentInfo() ? null :
-                  <PaymentInfoPanel
-                    removeCardHandler={this.removeCard.bind(this)}
-                    paymentInfo={this.getPaymentInfo()}
-                  />
+                { !this.getPaymentInfo()
+                  ? nulll
+                  : <PaymentInfoPanel
+                      removeCardHandler={this.removeCard.bind(this)}
+                      paymentInfo={this.getPaymentInfo()}
+                    />
                 }
               </div>
             </div>
@@ -290,4 +290,33 @@ export default class Billing extends Component {
       </div>
     );
   }
+}
+
+function getDates(loading, paymentProcessor) {
+  const today = new Date();
+  const billingDate =
+    (loading || !paymentProcessor)
+    ? today.getDate()
+    : paymentProcessor.billingDate;
+  const daysInMonth = (new Date(
+    today.getFullYear(),
+    (today.getMonth() - 1),
+    0
+  )).getDate();
+  const startDayOfMonth =
+    (billingDate > daysInMonth)
+    ? daysInMonth
+    : billingDate;
+  const startDate = Date.parse(new Date(
+    today.getFullYear(),
+    (today.getMonth() - 1),
+    startDayOfMonth
+  ));
+
+  const endDate = (moment(startDate).add('1', 'month').unix() * 1000);
+
+  return {
+    startDate,
+    endDate
+  };
 }
