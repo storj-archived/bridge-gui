7cbeaafbc45373b8076ce83de3c0cdf4a5afacc1
Barbara Liau <bj.liau@gmail.com>
2016-11-08 13:40:01 -0700

    Space linting

--- src/containers/auth/login-form/login-validation.js
+++ src/containers/auth/login-form/login-validation.js
@@ -1,10 +1,10 @@
 import * as validate from 'utils/validation';
 
-export default function({email, password}) {
+export default function({ email, password }) {
   const errors = {};
 
   errors.email = validate.email(email) || validate.required(email);
   errors.password = validate.required(password);
 
   return errors;
-};
+}
