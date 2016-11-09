bf072cd5be4598ff4ce44ef0cc60255ee68120db
Barbara Liau <bj.liau@gmail.com>
2016-11-08 17:43:03 -0700

    Spacing, adding propTypes, following the demands of eslint to arrange functions appropriately

--- src/containers/buckets/new-bucket.js
+++ src/containers/buckets/new-bucket.js
@@ -1,9 +1,8 @@
-import React, {Component, PropTypes} from 'react';
-import {connect} from 'react-redux';
-import {bindActionCreators} from 'redux';
-import {reduxForm} from 'redux-form';
+import React, { Component, PropTypes } from 'react';
+import { connect } from 'react-redux';
+import { reduxForm } from 'redux-form';
 import * as bucketActions from 'redux/modules/bucket';
-import {Link, hashHistory} from 'react-router';
+import { Link, hashHistory } from 'react-router';
 import bucketFormValidation from 'containers/buckets/bucket-form-validation';
 
 @connect(
@@ -27,12 +26,22 @@ import bucketFormValidation from 'containers/buckets/bucket-form-validation';
 })
 
 export default class NewBucket extends Component {
-
   static propTypes = {
     fields: PropTypes.object.isRequired, // from redux-form
     create: PropTypes.func.isRequired,
-    handleSubmit: PropTypes.func.isRequired
+    handleSubmit: PropTypes.func.isRequired,
+    clear: PropTypes.func
   };
+
+  componentWillReceiveProps(nextProps) {
+    if (nextProps.bucket.saved === true) {
+      hashHistory.push('/dashboard/bucket/' + nextProps.bucket.id + '/files');
+    }
+  }
+
+  componentWillUnmount() {
+    this.props.clear();
+  }
 
   createBucket() {
     const {
@@ -45,24 +54,12 @@ export default class NewBucket extends Component {
     });
   }
 
-  componentWillReceiveProps(nextProps) {
-    if (nextProps.bucket.saved === true) {
-      hashHistory.push('/dashboard/bucket/' + nextProps.bucket.id + '/files');
-    }
-  }
-
-  componentWillUnmount() {
-    this.props.clear();
-  }
-
   render() {
-    let {query} = this.props.location
     return (
       <section>
         <div className="container">
           <div className="row">
             <div className="col-xs-12">
-
               <div className="row">
                 <div className="col-sm-12">
                   <h1 className="title pull-left">Create Bucket</h1>
@@ -75,8 +72,13 @@ export default class NewBucket extends Component {
                     <div className="content">
                       <div className="form-group">
                         <label htmlFor="name">Bucket Name</label>
-                        <input type="text" className="form-control" name="name"
-                               placeholder="Bucket Name" {...this.props.fields.name}/>
+                        <input
+                          type="text"
+                          className="form-control"
+                          name="name"
+                          placeholder="Bucket Name"
+                          {...this.props.fields.name}
+                        />
                       </div>
                     </div>
                   </div>
@@ -84,12 +86,23 @@ export default class NewBucket extends Component {
 
                 <div className="row">
                   <div className="col-xs-6">
-                    <Link to="/dashboard" className="btn btn-block btn-transparent">Go Back</Link>
+                    <Link
+                      to="/dashboard"
+                      className="btn btn-block btn-transparent"
+                    >
+                      Go Back
+                    </Link>
                   </div>
 
                   <div className="col-xs-6">
-                    <a href="#noop" onClick={this.props.handleSubmit(this.createBucket.bind(this))}
-                       className="btn btn-block btn-green btn-create-bucket">Save Bucket</a>
+                    <a
+                      href="#noop"
+                      onClick={this.props
+                        .handleSubmit(this.createBucket.bind(this))}
+                      className="btn btn-block btn-green btn-create-bucket"
+                    >
+                      Save Bucket
+                    </a>
                   </div>
                 </div>
               </form>
