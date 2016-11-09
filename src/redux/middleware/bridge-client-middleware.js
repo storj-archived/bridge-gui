f170d52a2c0bcbb7f73868fa39e67007d8a6a0e9
Barbara Liau <bj.liau@gmail.com>
2016-11-08 18:05:19 -0700

    Space linting

--- src/redux/middleware/bridge-client-middleware.js
+++ src/redux/middleware/bridge-client-middleware.js
@@ -1,30 +1,30 @@
 export default function bridgeClientMiddleware({bridgeClient, apolloClient}) {
-  return ({dispatch, getState}) => {
+  return ({ dispatch, getState }) => {
     return next => action => {
       if (typeof action === 'function') {
         return action(dispatch, getState);
       }
       // eslint-disable-line no-redeclare
-      const {promise, types, ...rest} = action;
+      const { promise, types, ...rest } = action;
 
       if (!promise) {
         return next(action);
       }
 
       const [REQUEST, SUCCESS, FAILURE] = types;
-      next({...rest, type: REQUEST});
+      next({ ...rest, type: REQUEST });
 
-      const actionPromise = promise({bridgeClient, apolloClient});
+      const actionPromise = promise({ bridgeClient, apolloClient });
       actionPromise.then(
         (result) => {
-          next({...rest, result, type: SUCCESS});
+          next({ ...rest, result, type: SUCCESS });
         },
         (error) => {
-          next({...rest, error, type: FAILURE});
+          next({ ...rest, error, type: FAILURE });
         }
       ).catch((error) => {
         console.error('MIDDLEWARE ERROR:', error);
-        next({...rest, error, type: FAILURE});
+        next({ ...rest, error, type: FAILURE });
       });
 
       return actionPromise;
