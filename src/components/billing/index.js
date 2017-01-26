a7a2ecdd2f14bc328ed999334b54f8c7ed975675
Bryan White <bryanchriswhite@gmail.com>
2017-01-26 09:01:56 +0100

    STEP 2 OF 2: change case of src/components/billing

---
+++ src/components/billing/index.js
@@ -0,0 +1,4 @@
+import BalancePanel from 'components/billing/balance-panel';
+import UsagePanel from 'components/billing/usage-panel';
+
+export default { BalancePanel, UsagePanel };
---
+++ src/components/billing/transactions-list.js
@@ -0,0 +1,56 @@
+import React from 'react';
+import Currency from 'components/billing/currency';
+
+const TransactionsList = ({transactions}) => {
+  return (
+    <section id="TransactionsListSection">
+      <div className="container">
+        <div className="row">
+          <div className="col-xs-12">
+            <h2 className="title">Billing History</h2>
+              </div>
+            </div>
+            <div className="row">
+          <div className="col-xs-12">
+            <div className="table-responsive content">
+              <table className="table table-hover">
+                <thead>
+                  <tr>
+                    <th>Date</th>
+                    <th>Description</th>
+                    <th>Amount</th>
+                  </tr>
+                </thead>
+                  <tbody>
+                    {
+                      transactions.map((transaction) => {
+                        const isNegative = (transaction.amount < 0);
+                        return (
+                          <tr key={transaction.id} className="clickable-row">
+                            <td>
+                              {transaction.created}
+                            </td>
+                            <td>
+                              {transaction.description}
+                            </td>
+                            <td>
+                              <span className={isNegative ? 'text-success' : ''}>
+                                {isNegative ? '-' : ''}
+                                <Currency amount={isNegative ? -transaction.amount : transaction.amount} />
+                              </span>
+                            </td>
+                          </tr>
+                        );
+                      })
+                    }
+                </tbody>
+              </table>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default TransactionsList;
