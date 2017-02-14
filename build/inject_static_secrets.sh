021b03c064385f18eda2f04dd88ab1f301064fda
Philip Hutchins <flipture@gmail.com>
2017-02-14 12:46:22 -0500

    Updates to start script wrapper and var parser

--- build/inject_static_secrets.sh
+++ build/inject_static_secrets.sh
@@ -4,6 +4,7 @@ STATIC_PATH=./static/
 ENVVARS=$@
 
 function dosub {
+  echo "Replacing $1 in all static content starting at path $STATIC_PATH with $2"
   find $STATIC_PATH -type f -print0 | LC_ALL=C xargs -0 sed -i "s/ENV_${1}/\$${2}/g"
 }
 
