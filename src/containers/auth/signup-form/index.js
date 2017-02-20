c32d683962b80bb1afe2b475994ad2180ce8fdf2
Tome Boshevski <tome@boshevski.com>
2017-02-20 13:10:46 -0500

    Update ToS modal - Fix #105

--- src/containers/auth/signup-form/index.js
+++ src/containers/auth/signup-form/index.js
@@ -74,7 +74,8 @@ export default class SignUpForm extends Component {
 
   renderEula() {
     return (
-      <Modal show={this.state.showEula} onHide={this.closeEula.bind(this)} bsSize="large" className="modal-scroll-body">
+      <
+      l show={this.state.showEula} onHide={this.closeEula.bind(this)} bsSize="large">
         <Modal.Header closeButton><h1 className="text-center">Storj Labs</h1></Modal.Header>
         <Modal.Body>
           <TermsOfService/>
