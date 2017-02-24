a99e96dfdec87f4b7b93344b9a00bf8a47e0b8f1
Barbara Liau <bj.liau@gmail.com>
2017-02-23 18:47:18 -0700

    Add spacing between sentences

--- src/components/referrals/referral-info.js
+++ src/components/referrals/referral-info.js
@@ -9,12 +9,11 @@ export default class ReferralInfo extends Component {
         >
           <div className="spacer20"></div>
           <h2>Give 50GB, Get 50GB</h2>
-          <p>
-            Give your friends 50GB of storage and 50GB of bandwidth per month
+          <p> Give your friends 50GB of storage and 50GB of bandwidth per month
             on Storj, for the next three months — all on us.
-            <br/> After they've spent $10 on Storj, we'll give you the same.
-            <br/>There is no limit to how many friends you can refer.
           </p>
+          <p> After they've spent $10 on Storj, we'll give you the same. </p>
+          <p> There is no limit to how many friends you can refer. </p>
           <div className="spacer40"></div>
         </div>
       </div>
