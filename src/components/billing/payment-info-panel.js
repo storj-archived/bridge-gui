cd8f0e8a4d273fb927be735c6cbde925dbdcb15e
Barbara Liau <bj.liau@gmail.com>
2017-03-09 21:33:37 -0700

    Move HTML here so logic on billing/index.js is cleaner

--- src/components/billing/payment-info-panel.js
+++ src/components/billing/payment-info-panel.js
@@ -5,27 +5,33 @@ const PaymentInfoPanel = ({
   paymentInfo: {merchant, lastFour}
 }) => {
   return (
-    <div>
-      <h2 className="title">Default Payment Method</h2>
-      <div className="content">
+    <section>
+      <div className="container">
         <div className="row">
-          <div className="col-xs-4">
-            <p className="text-muted">Merchant</p>
-            <h2>{merchant}</h2>
-          </div>
-          <div className="col-xs-4">
-            <p className="text-muted">Card Number</p>
-            <h2>**** **** **** {lastFour}</h2>
-          </div>
-          <div className="col-xs-4 text-right">
-            <button onClick={removeCardHandler}
-              className="btn btn-action btn-red">
-                Remove Card
-            </button>
+          <div className="col-xs-12">
+            <h2 className="title">Default Payment Method</h2>
+            <div className="content">
+              <div className="row">
+                <div className="col-xs-4">
+                  <p className="text-muted">Merchant</p>
+                  <h2>{merchant}</h2>
+                </div>
+                <div className="col-xs-4">
+                  <p className="text-muted">Card Number</p>
+                  <h2>**** **** **** {lastFour}</h2>
+                </div>
+                <div className="col-xs-4 text-right">
+                  <button onClick={removeCardHandler}
+                    className="btn btn-action btn-red">
+                      Remove Card
+                  </button>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </div>
-    </div>
+    </section>
   );
 };
 
