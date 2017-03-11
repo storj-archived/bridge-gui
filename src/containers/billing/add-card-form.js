f59cddd14c61561fb4e057d36e4c00592fd915d7
Barbara Liau <bj.liau@gmail.com>
2017-03-10 19:53:00 -0700

    Display errors and improve handling when calling addPaymentMethod

--- src/containers/billing/add-card-form.js
+++ src/containers/billing/add-card-form.js
@@ -133,10 +133,30 @@ export default class AddCardForm extends Component {
       exp_year,
       cvc: cvv.value
     }, (status, response) => {
+      if (response.error) {
+        this.setState({ submitting: false });
+        this.setState({
+          submitError: `Processing error: ${response.error.message}`
+        });
+        return;
+      }
+
       const token = response.id;
-      this.props.mutations.addPaymentMethod(JSON.stringify(token))
-        .then(() => {
+      this.props.mutations.addPaymentMethod(JSON.stringify(token), 'stripe')
+        .then((result) => {
+          this.setState({ submitting: false });
+          if (result.errors) {
+            this.setState({
+              submitError: `Error adding payment method: ${result.errors}`
+            });
+            return;
+          }
           this.props.updatePaymentInfo();
+        })
+        .catch((err) => {
+          console.log('err submitting', err);
+          this.setState({ submitting: false });
+          this.setState({ submitError: err.message });
         });
     });
 
