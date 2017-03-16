0d57fd50a6b53b68ae0a08748184a6b13ac8fc7a
Barbara Liau <bj.liau@gmail.com>
2017-03-16 16:51:34 -0600

    Add prettifyAmount method to prettify amount

--- src/components/billing/currency.js
+++ src/components/billing/currency.js
@@ -7,13 +7,25 @@ import React from 'react';
  * This may likely be done based on locale.
  */
 
-const Currency = ({amount}) => {
-  const value = (typeof(amount) !== 'number') ?
-    '' : (amount / 100).toFixed(6);
+const Currency = ({ amount }) => {
+  const value = prettifyAmount(amount);
 
   return (
     <span>${value}</span>
   );
 };
+
+function prettifyAmount(amount) {
+  if (typeof(amount) !== 'number') {
+    return '0.00';
+  }
+  const modAmount = (amount / 100).toFixed(6);
+
+  if (modAmount.indexOf('0.000000') === 0) {
+    return '0.00'
+  }
+
+  return modAmount;
+}
 
 export default Currency;
