ceabac3d5a29af27a86b617de1e675e251ed4227
Barbara Liau <bj.liau@gmail.com>
2017-03-16 16:24:18 -0600

    Move CSS classNames here

--- src/components/billing/balance-panel.js
+++ src/components/billing/balance-panel.js
@@ -3,24 +3,24 @@ import Currency from 'components/billing/currency';
 
 const BalancePanel = ({balance, addCreditHandler}) => {
   return (
-    <div>
+    <div className="col-xs-12 col-sm-6">
       <h2 className="title">Your Balance</h2>
       <div className="content">
         <div className="row">
           <div className="col-xs-6">
             <p className="text-muted">Current Balance</p>
             <h2 className="mb0 blue"><b>
-              <Currency balance={balance} />
+              <Currency amount={balance} />
             </b></h2>
           </div>
-          <div className="col-xs-6 text-right">
+          {/* <div className="col-xs-6 text-right">
             <button
               disabled={balance <= 0}
               onClick={addCreditHandler}
               className="btn btn-green btn-action" >
                 Pay Balance
             </button>
-          </div>
+          </div> */}
         </div>
       </div>
     </div>
--- src/components/billing/usage-panel.js
+++ src/components/billing/usage-panel.js
@@ -3,31 +3,37 @@ import {Link} from 'react-router';
 import Currency from 'components/billing/currency';
 import './usage-panel.scss';
 
-const UsagePanel = ({storage, bandwidth}) => {
+const UsagePanel = ({ storage, bandwidth }) => {
   return (
-    <div>
+    <div className="col-xs-12 col-sm-6">
       <h2 className="title">Usage This Month</h2>
       <div className="content">
         <div className="row">
           <div className="col-xs-6">
-            <h2 className="">
+            <div className="">
               <h4>Storage</h4>
               <span className="mb0 blue">{storage}</span>
-              <div className="text-muted unit-text" style={{display: 'inline-block'}}>
+              <div
+                className="text-muted unit-text"
+                style={{ display: 'inline-block' }}
+              >
                 <div> / 25GBh</div>
                 <div>free</div>
               </div>
-            </h2>
+            </div>
           </div>
           <div className="col-xs-6">
-            <h2 className="mb0">
+            <div className="mb0">
               <h4>Bandwidth</h4>
               <span className="mb0 blue">{bandwidth}</span>
-              <div className="text-muted unit-text" style={{display: 'inline-block'}}>
+              <div
+                className="text-muted unit-text"
+                style={{ display: 'inline-block' }}
+              >
                 <div> / 25GB</div>
                 <div>free</div>
               </div>
-            </h2>
+            </div>
           </div>
         </div>
       </div>
